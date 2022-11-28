794d25091196363b0ef4f70e028575e3ba72a091
nsainton <noahsaintonge@gmail.com>
2022-11-28 22:38:42 +0100

    debugged version of binsum

--- Code/binsum.c
+++ Code/binsum.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 05:41:34 by nsainton          #+#    #+#             */
-/*   Updated: 2022/11/28 06:00:25 by nsainton         ###   ########.fr       */
+/*   Updated: 2022/11/28 22:37:24 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,34 +31,38 @@ void	ft_print_memory(int *tab, size_t size)
 
 int	*ft_sum(int *A, int *B, size_t size)
 {
-	int	i;
-	int	*C;
+	int		i;
+	int		*C;
+	int		tmp;
 
 	if (A == NULL || B == NULL || ! size)
 		return (NULL);
 	C = malloc(sizeof *C * (size + 1));
 	if (C == NULL)
 		return (NULL);
-	C[size] = (A[size - 1] + B[size - 1]) % 2;
-	C[size - 1] = (A[size - 1] + B[size - 1]) / 2;
-	i = size - 2;
+	C[size] = 0;
+	i = (int)size - 1;
 	while (i > -1)
 	{
-		*(C + i + 1) = (*(A + i) + *(B + i)) / 2;
-		*(C + i + 1) = (*(A + i) + *(B + i)) % 2;
-		i ++;
+		tmp = A[i] + B[i] + C[i + 1];
+		*(C + i) = tmp / 2;
+		*(C + i + 1) = tmp % 2;
+	//	printf("C[%d] : %d\nC[%d] : %d\n", i + 1, C[i + 1], i, C[i]);
+		i --;
 	}
 	return (C);
 }
 
 int	main(void)
 {
-	int	A[10] = {0, 1, 1, 0, 1, 1, 0, 1, 1, 0};
-	int	B[10] = {1, 1, 1, 0, 1, 0, 0, 0, 1, 1};
-	int	*C;
+	int		A[10] = {0, 1, 1, 0, 1, 1, 0, 1, 1, 0};
+	int		B[10] = {1, 1, 1, 0, 1, 0, 0, 0, 1, 1};
+	int		*C;
+	size_t	size;
 
-	C = ft_sum(A, B, 10);
-	ft_print_memory(C, 11);
+	size = sizeof A / sizeof *A;
+	C = ft_sum(A, B, size);
+	ft_print_memory(C, size + 1);
 	if (C)
 		free(C);
 	return (0);
